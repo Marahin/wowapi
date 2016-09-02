4a74532028f7773e285a39e310c4b8a5c58967e5
Jan Matusz <jan.matusz@unitedideas.pl>
2016-09-03 00:38:36 +0200

    Proper naming for secret_key (instead of private key)

--- lib/wowapi/exceptions/no_credentials_exception.rb
+++ lib/wowapi/exceptions/no_credentials_exception.rb
@@ -1,6 +1,6 @@
 class Wowapi
   class NoCredentialsException < Exception
-    def initialize(msg = 'Public and/or private key was not provided.')
+    def initialize(msg = 'Public and/or secret key was not provided.')
       super
     end
   end
