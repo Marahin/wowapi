972cf0da41d7dfd6e80ad89a3c88b91f93bdb1af
Jan Matusz <jan.matusz@unitedideas.pl>
2016-09-03 00:38:55 +0200

    Make sure .guild method returns JSON

--- lib/wowapi/modules/guild.rb
+++ lib/wowapi/modules/guild.rb
@@ -1,9 +1,15 @@
 class Wowapi
   module Modules
     module Guild
+      # this should probably include
+      # a GuildClass definition which would properly
+      # handle blizzard apis' response.
+      # for now it returns just a simple JSON
+      # (also, written while drunk. sorry. )
       def guild(realm, name, *args)
         args = args.map{|n| n.to_s if n.is_a?(Symbol) }
-        get 'guild/', "#{realm}/#{name}?fields=#{args.join('%2C')}"
+        res = get 'guild/', "#{realm}/#{name}?fields=#{args.join('%2C')}"
+        JSON.parse(res)
       end
     end
   end
