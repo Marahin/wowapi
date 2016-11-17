3fff6b3e1515911467d6157072dd98ac36bac56d
marahin <me@marahin.pl>
2016-11-17 23:06:07 +0100

    Fix #16: made 'thumbnail' field on CharacterClass work on both Guild and Character API calls

--- lib/wowapi/modules/character.rb
+++ lib/wowapi/modules/character.rb
@@ -1,7 +1,6 @@
 class Wowapi
   module Modules
-    # Character module containing methods for
-    # character data retrieval & definition of
+    # Character module containing methods for character data retrieval & definition of
     # CharacterClass
     module Character
       # todo: issue-13
@@ -9,8 +8,15 @@ class Wowapi
         def initialize(res)
           super
 
+          # Likely requires refactoring. This reassigns unsufficient thumbnail address
+          # returned by World of Warcraft API and sets it to proper thumbnail URL
+          # that you can use to download / display image from Blizzards' servers
           if @table
-            @table[:character]['thumbnail'] = "http://render-api-#{Wowapi.region}.worldofwarcraft.com/static-render/#{Wowapi.region}/#{@table[:character]['thumbnail']}"
+            if @table.key?(:character) && @table[:character].key?('thumbnail')
+              @table[:character]['thumbnail'] = "http://render-api-#{Wowapi.region}.worldofwarcraft.com/static-render/#{Wowapi.region}/#{@table[:character]['thumbnail']}"
+            elsif @table.key?(:thumbnail)
+              @table[:thumbnail] = "http://render-api-#{Wowapi.region}.worldofwarcraft.com/static-render/#{Wowapi.region}/#{@table[:thumbnail]}"
+            end
           end
         end
 
@@ -20,8 +26,7 @@ class Wowapi
         end
       end
 
-      # Retrieve data about particular Character
-      # For a list of fields visit README.md
+      # Retrieve data about particular Character. For a list of fields visit README.md
       def character(realm, name, *args)
         args = args.map{|n| n if n.is_a?(Symbol) }
         res = get 'character/', "#{realm}/#{name}?fields=#{args.join(',')}"
